fdc498500a3cb19e926147ef0282799e46957468
IWantBeProgramer <lol6660.studios@gmail.com>
2023-03-23 00:44:45 +0300

    Added DifferenceViewFlipper

---
+++ app/src/main/java/android/widget/DifferenceViewFlipper.java
@@ -0,0 +1,32 @@
+package android.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * It's ViewFlipper, but setDisplayedChild() works only if whichChild
+ * difference by already displayed child
+ */
+public class DifferenceViewFlipper extends ViewFlipper {
+
+    public DifferenceViewFlipper(Context context) {
+        super(context);
+    }
+
+    public DifferenceViewFlipper(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * Sets which child view will be displayed.
+     *
+     * @param whichChild the index of the child view to display
+     */
+    @Override
+    public void setDisplayedChild(int whichChild) {
+        if (getDisplayedChild() != whichChild) {
+            super.setDisplayedChild(whichChild);
+        }
+    }
+
+}

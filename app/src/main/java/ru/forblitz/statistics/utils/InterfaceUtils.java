c96923f6ee259f4cce2f33a2791f466b9182766d
IWantBeProgramer <lol6660.studios@gmail.com>
2023-04-05 17:29:41 +0300

    allActivatedCheck() and its uses have been removed

--- app/src/main/java/ru/forblitz/statistics/utils/InterfaceUtils.java
+++ app/src/main/java/ru/forblitz/statistics/utils/InterfaceUtils.java
@@ -11,7 +11,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.widget.EditText;

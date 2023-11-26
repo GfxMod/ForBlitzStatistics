bcdf43c09e3da4a171c6f060a5d4e3250a7ce0cd
GfxMod <lol6660.studios@gmail.com>
2023-11-26 14:30:53 +0300

    PlayerFastStat.setData() method has been refactored

--- app/src/main/java/ru/forblitz/statistics/widget/data/PlayerFastStat.kt
+++ app/src/main/java/ru/forblitz/statistics/widget/data/PlayerFastStat.kt
@@ -1,5 +1,6 @@
 package ru.forblitz.statistics.widget.data
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
@@ -26,78 +27,77 @@ class PlayerFastStat : LinearLayout {
     private val averageDamageRounding: Boolean
         get() = (context.applicationContext as ForBlitzStatisticsApplication).preferencesService.get(PreferencesSwitchesTags.averageDamageRounding)
 
+    @SuppressLint("SetTextI18n")
     fun setData(nickname: String, statisticsData: StatisticsData) {
-        val battles = findViewWithTag<TextView>("battles")
-        val winRate = findViewWithTag<TextView>("winRate")
-        val averageDamage = findViewWithTag<TextView>("averageDamage")
-        val efficiency = findViewWithTag<TextView>("efficiency")
-        val survived = findViewWithTag<TextView>("survived")
-        val hitsFromShots = findViewWithTag<TextView>("hitsFromShots")
-
-        val battlesValue = ParseUtils.splitByThousands(statisticsData.battles.toString())
-        var winningPercentageValue = "${statisticsData.winningPercentage!!.round()}%"
-        var averageDamageValue = ParseUtils.splitByThousands(
-            if (!averageDamageRounding) {
-                statisticsData.averageDamage!!.toInt().toString()
-            } else {
-                statisticsData.averageDamage!!.round().toString()
-            }
-        )
-        var efficiencyValue = statisticsData.efficiency!!.round().toString()
-        var survivedPercentageValue = statisticsData.survivedPercentage!!.round().toString()
-        var hitsOutOfShotsValue = statisticsData.hitsOutOfShots!!.round().toString()
-
-        var winningPercentageColor = InterfaceUtils.getValueColor(
-            this.context,
-            statisticsData.winningPercentage!!.toDouble(),
-            Constants.Steps.winRate
-        )
-        var averageDamageColor = InterfaceUtils.getValueColor(
-            this.context,
-            statisticsData.averageDamage!!.toDouble(),
-            Constants.Steps.averageDamage
-        )
-        var efficiencyColor = InterfaceUtils.getValueColor(
-            this.context,
-            statisticsData.efficiency!!.toDouble(),
-            Constants.Steps.efficiency
-        )
-
-        if (statisticsData.winningPercentage!!.isNaN()) {
-            winningPercentageValue = context.getString(R.string.empty)
-            winningPercentageColor = this.context.getColor(R.color.white)
-        }
-        if (statisticsData.averageDamage!!.isNaN()) {
-            averageDamageValue = context.getString(R.string.empty)
-            averageDamageColor = this.context.getColor(R.color.white)
-        }
-        if (statisticsData.efficiency!!.isNaN()) {
-            efficiencyValue = context.getString(R.string.empty)
-            efficiencyColor = this.context.getColor(R.color.white)
-        }
-        if (statisticsData.survivedPercentage!!.isNaN()) {
-            survivedPercentageValue = context.getString(R.string.empty)
-        }
-        if (statisticsData.hitsOutOfShots!!.isNaN()) {
-            hitsOutOfShotsValue = context.getString(R.string.empty)
-        }
-
-        battles.text = battlesValue
-        winRate.text = winningPercentageValue
-        averageDamage.text = averageDamageValue
-        efficiency.text = efficiencyValue
-        survived.text = survivedPercentageValue
-        hitsFromShots.text = hitsOutOfShotsValue
-        battles.setTextColor(
-            InterfaceUtils.getValueColor(
-                this.context,
-                statisticsData.battles.toDouble(),
-                Constants.Steps.battles
+        with(findViewWithTag<TextView>("battles")) {
+            text = ParseUtils.splitByThousands(statisticsData.battles.toString())
+            setTextColor(
+                InterfaceUtils.getValueColor(
+                    this.context,
+                    statisticsData.battles.toDouble(),
+                    Constants.Steps.battles
+                )
             )
-        )
-        winRate.setTextColor(winningPercentageColor)
-        averageDamage.setTextColor(averageDamageColor)
-        efficiency.setTextColor(efficiencyColor)
+        }
+        with(findViewWithTag<TextView>("winRate")) {
+            if (statisticsData.winningPercentage!!.isNaN()) {
+                text = context.getString(R.string.empty)
+                setTextColor(context.getColor(R.color.white))
+            } else {
+                text = "${statisticsData.winningPercentage!!.round()}%"
+                setTextColor(InterfaceUtils.getValueColor(
+                    context,
+                    statisticsData.winningPercentage!!.toDouble(),
+                    Constants.Steps.winRate
+                ))
+            }
+        }
+        with(findViewWithTag<TextView>("averageDamage")) {
+            if (statisticsData.averageDamage!!.isNaN()) {
+                text = context.getString(R.string.empty)
+                setTextColor(context.getColor(R.color.white))
+            } else {
+                text = ParseUtils.splitByThousands(
+                    if (!averageDamageRounding) {
+                        statisticsData.averageDamage!!.toInt().toString()
+                    } else {
+                        statisticsData.averageDamage!!.round().toString()
+                    }
+                )
+                setTextColor(InterfaceUtils.getValueColor(
+                    this.context,
+                    statisticsData.averageDamage!!.toDouble(),
+                    Constants.Steps.averageDamage
+                ))
+            }
+        }
+        with(findViewWithTag<TextView>("efficiency")) {
+            if (statisticsData.efficiency!!.isNaN()) {
+                text = context.getString(R.string.empty)
+                setTextColor(context.getColor(R.color.white))
+            } else {
+                text = statisticsData.efficiency!!.round().toString()
+                setTextColor(InterfaceUtils.getValueColor(
+                    this.context,
+                    statisticsData.efficiency!!.toDouble(),
+                    Constants.Steps.efficiency
+                ))
+            }
+        }
+        with(findViewWithTag<TextView>("survived")) {
+            text = if (statisticsData.survivedPercentage!!.isNaN()) {
+                context.getString(R.string.empty)
+            } else {
+                statisticsData.survivedPercentage!!.round().toString()
+            }
+        }
+        with(findViewWithTag<TextView>("hitsFromShots")) {
+            text = if (statisticsData.hitsOutOfShots!!.isNaN()) {
+                context.getString(R.string.empty)
+            } else {
+                statisticsData.hitsOutOfShots!!.round().toString()
+            }
+        }
         findViewWithTag<TextView>("nickname").text = nickname
     }
 

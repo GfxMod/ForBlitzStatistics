f657c41842a60f0d9f5d44caf739c5681f2d49c0
GfxMod <lol6660.studios@gmail.com>
2023-12-07 22:12:44 +0300

    Added verification that the imported sessions are indeed sessions

--- app/src/main/java/ru/forblitz/statistics/helpers/SessionsIOHelper.kt
+++ app/src/main/java/ru/forblitz/statistics/helpers/SessionsIOHelper.kt
@@ -4,18 +4,27 @@ import android.content.Context
 import android.content.Intent
 import android.os.Environment
 import android.provider.DocumentsContract
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import com.google.gson.Gson
+import com.google.gson.JsonParser
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import ru.forblitz.statistics.R
+import ru.forblitz.statistics.dto.UserStatisticsResponse
 import ru.forblitz.statistics.utils.ZipUtils
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
 
 
 class SessionsIOHelper {
     companion object {
 
         private const val TMP_FILE_NAME = "tmp.fbss"
+
+        private const val TMP_DIR_NAME = "tmp_sessions"
 
         private val externalDirectory = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "ForBlitzStatistics")
 
@@ -39,10 +48,31 @@ class SessionsIOHelper {
         }
 
         private fun importFBSS(context: Context, fbssFile: File) {
+            val tmpDir = File(sessionsDirectory(context), TMP_DIR_NAME)
+            with(tmpDir) {
+                if (exists()) {
+                    deleteRecursively()
+                }
+                mkdir()
+            }
             ZipUtils.unpackDir(
                 fbssFile,
-                sessionsDirectory(context)
+                tmpDir
             )
+            tmpDir.listFiles()?.forEach { file ->
+                // try to parse file as statistics
+                // it throw exception if it not statistics
+                Gson().fromJson(
+                    JsonParser
+                        .parseString(file.readText())
+                        .asJsonObject,
+                    UserStatisticsResponse::class.java)
+                // we can move file to
+                Files.move(
+                    file.toPath(), File(sessionsDirectory(context), file.name).toPath(), StandardCopyOption.REPLACE_EXISTING
+                )
+            }
+            tmpDir.delete()
         }
 
         fun importSessionsWithPicker(
@@ -54,14 +84,20 @@ class SessionsIOHelper {
                 activityResult.data.also { pickedFile ->
                     if (pickedFile != null) {
                         CoroutineScope(Dispatchers.IO).launch {
-                            File(sessionsDirectory(context), TMP_FILE_NAME).apply tmpFBSS@ {
-                                outputStream().use { fileOutputStream ->
-                                    context.contentResolver.openInputStream(pickedFile)!!.use { input ->
-                                        input.copyTo(fileOutputStream)
+                            try {
+                                File(sessionsDirectory(context), TMP_FILE_NAME).apply tmpFBSS@ {
+                                    outputStream().use { fileOutputStream ->
+                                        context.contentResolver.openInputStream(pickedFile)!!.use { input ->
+                                            input.copyTo(fileOutputStream)
+                                        }
                                     }
+                                    importFBSS(context, this@tmpFBSS)
+                                    delete()
                                 }
-                                importFBSS(context, this@tmpFBSS)
-                                delete()
+                            } catch (e: Exception) {
+                                CoroutineScope(Dispatchers.Main).launch {
+                                    Toast.makeText(context, context.getString(R.string.sessions_io_import_error, e.javaClass), Toast.LENGTH_SHORT).show()
+                                }
                             }
                         }
                     }
